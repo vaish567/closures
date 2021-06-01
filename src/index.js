d19ac0885193741a55c38cfc5a00295cf27ef823
Vaishnav <60536883+vaish567@users.noreply.github.com>
2021-06-01 15:59:42 +0530

    Made some minor changes: adding up commentss

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react"; // we want latest value we are using "useRef" hook
 import ReactDOM from "react-dom";
 
 function App() {
   const [count, setCount] = useState(0);
-  const countRef = useRef(0);
+  const countRef = useRef(0); // idea here is I'm going to store the value of count inside of "countRef" reference. AlsoI have set default value "0"
 
+  // as you can see I'm keeping track of reference "countRef.current" and it is same reference across the value, also I'm updating value here is the reference
   countRef.current = count;
 
+  // So here in useEffect I might connect to WebSocket or maybe connecting to an API, maybe async operation
+  // But Basically here I'm doing async operation on the first render, in this creating "setTimeOut" and displaying what the count is.
+  // Getting value from count at the current point "countRef.current"
   useEffect(() => {
     setTimeout(() => {
-      alert("Don't ping me " + countRef.current);
+      alert("Don't ping me " + countRef.current); // which is grabbing reference from here so that's key reference we're getting a reference to the value
     }, 3000);
   }, []);
 
